da262400b5cdc99460064ed50aaef95a8fe6b1bd
adinathbiradar <adinathbiradar77@gmail.com>
2024-06-04 15:52:43 +0530

    commit code

---
+++ src/main/java/in/codetech/angulareducloud/portal/AngularEduCloudApplication.java
@@ -0,0 +1,14 @@
+package in.codetech.angulareducloud.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularEduCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularEduCloudApplication.class, args);
+		System.out.println("Application");
+	}
+
+}
